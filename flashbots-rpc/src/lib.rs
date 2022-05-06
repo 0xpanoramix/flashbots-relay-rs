c7a53e376c585d64eb9945b6865cc8a3dea374c8
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-05-06 16:46:21 +0200

    Add requester base implementation
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- flashbots-rpc/src/lib.rs
+++ flashbots-rpc/src/lib.rs
@@ -1,8 +1,52 @@
+pub mod constants;
+
+use reqwest::{Client, ClientBuilder};
+
+#[derive(Clone, Debug)]
+pub struct Requester {
+    client: Client,
+    base_url: String,
+}
+
+impl Requester {
+    pub fn new(cfg: RequesterConfig) -> Self {
+        let builder = ClientBuilder::new();
+        let client = builder.build().unwrap();
+
+        Self {
+            client,
+            base_url: cfg.base_url,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct RequesterConfig {
+    base_url: String,
+}
+
+impl Default for RequesterConfig {
+    fn default() -> Self {
+        Self {
+            base_url: constants::DEFAULT_BASE_URL.parse().unwrap(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_can_create_requester_with_default_config() {
+        let _client = Requester::new(RequesterConfig::default());
+    }
+
+    #[test]
+    fn it_can_create_requester_with_custom_config() {
+        let cfg = RequesterConfig {
+            base_url: "".to_string(),
+        };
+        let _client = Requester::new(cfg);
     }
 }
