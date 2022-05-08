1979a619ef1a9f5dabe97d918ec04d77072b5af4
Luca Georges Francois <luca.georges-francois@epitech.eu>
2022-05-08 22:46:16 +0200

    Add support for flashbots_getBundleStats
    Signed-off-by: Luca Georges Francois <luca.georges-francois@epitech.eu>

--- flashbots-rpc/src/requester.rs
+++ flashbots-rpc/src/requester.rs
@@ -5,7 +5,7 @@ use serde_json::{json, Value};
 use sha3::{Digest, Keccak256};
 
 use crate::constants::{FLASHBOTS_AUTH_HEADER_NAME, FLASHBOTS_RELAY_RPC_ENDPOINT};
-use crate::types::UserStats;
+use crate::types::{BundleStats, FlashbotsGetBundleStatsParam, UserStats};
 
 #[derive(Clone, Debug)]
 pub struct Requester {
@@ -85,12 +85,52 @@ impl Requester {
         let user_stats: UserStats = response.json().await?;
         Ok(user_stats)
     }
+
+    pub async fn get_bundle_stats(
+        &self,
+        private_key: &str,
+        params: &FlashbotsGetBundleStatsParam,
+    ) -> Result<BundleStats, Box<dyn std::error::Error>> {
+        // Loads the ethereum wallet.
+        let wallet = private_key.parse::<LocalWallet>()?;
+
+        // Prepare the payload for POST request.
+        let request_payload = json!({
+            "id": 1,
+            "jsonrpc": "2.0",
+            "method": "flashbots_getBundleStats",
+            "params": [
+                params.block_hash,
+                params.block_number,
+            ],
+        });
+
+        // Sign the payload.
+        let payload_signature = self.sign_request_payload(&wallet, &request_payload).await?;
+        let signature = format!("{:#?}:0x{}", wallet.address(), payload_signature);
+
+        // Send the request.
+        let response = self
+            .client
+            .post(&self.base_url)
+            .header(FLASHBOTS_AUTH_HEADER_NAME, signature)
+            .json(&request_payload)
+            .send()
+            .await?;
+
+        // Parse the response and return the data.
+        let bundle_stats: BundleStats = response.json().await?;
+
+        println!("{:?}", bundle_stats);
+        Ok(bundle_stats)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::constants::FLASHBOTS_RELAY_RPC_ENDPOINT;
     use crate::requester::Requester;
+    use crate::types::FlashbotsGetBundleStatsParam;
 
     #[test]
     fn it_can_instantiate_requester_with_default_configuration() {
@@ -104,6 +144,19 @@ mod tests {
         let requester = Requester::default();
         let private_key = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7";
         let result = requester.get_user_stats(private_key, 14737875).await;
+
+        assert_eq!(result.is_err(), false);
+    }
+
+    #[tokio::test]
+    async fn it_can_get_bundle_stats() {
+        let requester = Requester::default();
+        let private_key = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7";
+        let params: FlashbotsGetBundleStatsParam = FlashbotsGetBundleStatsParam {
+            block_number: "0xe0e368".to_string(),
+            block_hash: "0x49628419847f55d9c5c9b912cdab27d916880a12f7f816cc69a0fd4b0c5430f3".to_string()
+        };
+        let result = requester.get_bundle_stats(private_key, &params).await;
 
         assert_eq!(result.is_err(), false);
     }
--- flashbots-rpc/src/types.rs
+++ flashbots-rpc/src/types.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 
+/// The response for flashbots_getUserStats.
 #[derive(Deserialize, Debug)]
 pub struct UserStats {
     pub is_high_priority: Option<bool>,
@@ -9,4 +10,33 @@ pub struct UserStats {
     pub last_7d_gas_simulated: Option<String>,
     pub last_1d_miner_payments: Option<String>,
     pub last_1d_gas_simulated: Option<String>,
+}
+
+/// The params used to query flashbots_getBundleStats.
+#[derive(Debug)]
+pub struct FlashbotsGetBundleStatsParam {
+    pub block_number: String,
+    pub block_hash: String,
+}
+
+/// The response for flashbots_getBundleStats.
+#[derive(Deserialize, Debug)]
+pub struct BundleStats {
+    #[serde(rename = "isSimulated")]
+    pub is_simulated: Option<bool>,
+
+    #[serde(rename = "isSentToMiners")]
+    pub is_sent_to_miners: Option<bool>,
+
+    #[serde(rename = "isHighPriority")]
+    pub is_high_priority: Option<bool>,
+
+    #[serde(rename = "simulatedAt")]
+    pub simulated_at: Option<String>,
+
+    #[serde(rename = "submittedAt")]
+    pub submitted_at: Option<String>,
+
+    #[serde(rename = "sentToMinersAt")]
+    pub sent_to_miners_at: Option<String>,
 }
